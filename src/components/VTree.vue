4cac0b9e8aafa9a80c795febf3388f0c5090c47b
XAHTEP26 <89197353165@mail.ru>
2018-11-10 00:58:52 +0300

    Initial commit

---
+++ src/components/VTree.vue
@@ -0,0 +1,10 @@
+<script>
+  export default {
+    name: 'VTree',
+    render() {
+      if (!this.$scopedSlots.default) this.$scopedSlots = this.$parent.$scopedSlots
+      return this.$scopedSlots.default ? this.$scopedSlots.default(this.scopeData) : null
+    },
+    props: ['scope-data'],
+  }
+</script>
